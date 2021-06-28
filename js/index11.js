fbe6e9ceb17b7a225e00b46f0a8852408390cbed
asqarDev <asqarmirzayev2@gmail.com>
2021-06-28 10:18:49 +0500

    project

---
+++ js/index11.js
@@ -0,0 +1,30 @@
+function checkboxBtn(){
+    var elemnts = document.getElementById('mybody');
+    elemnts.classList.toggle('mybodyww')
+    var navbar = document.getElementById('mynav');
+    navbar.classList.toggle('nav1')
+    var navlink=document.getElementById('navlink');
+    navlink.classList.toggle('mylink')
+    var navlink=document.getElementById('navlink2');
+    navlink.classList.toggle('mylink')
+    var navlink=document.getElementById('navlink3');
+    navlink.classList.toggle('mylink')
+    var dropdown = document.getElementById('dropdownMenuButton1');
+    dropdown.classList.toggle('button')
+    var dropdown = document.getElementById('dropdownMenuButton');
+    dropdown.classList.toggle('button')
+    var textp = document.getElementById('text1');
+    textp.classList.toggle('matn')
+    var textp = document.getElementById('text2');
+    textp.classList.toggle('matn')
+    var textp = document.getElementById('text3');
+    textp.classList.toggle('matn')
+    var textp = document.getElementById('text5');
+    textp.classList.toggle('matn')
+    var textp = document.getElementById('text4');
+    textp.classList.toggle('matn')
+    var textp = document.getElementById('text6');
+    textp.classList.toggle('matn')
+    var h5  = document.getElementById('h5');
+    h5.classList.toggle('h5')
+}
